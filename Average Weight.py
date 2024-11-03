55220b84b1789f1fb53ef964847c55749a74f1ea
GPardhasaradhireddy <147792497+GPardhasaradhireddy@users.noreply.github.com>
2024-11-03 20:33:33 +0530

    Uploading code: Average Weight.py

---
+++ Average Weight.py
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AverageWeight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int average = scanner.nextInt();
+        int W1 = scanner.nextInt();
+        int W2 = scanner.nextInt();
+        
+        int W3 = (3 * average) - (W1 + W2);
+        System.out.println(W3);
+    }
+}
